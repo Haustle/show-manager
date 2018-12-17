b5b93f1b4bf905cf199abe0619b7e2ef46e58aef
Tyrus <tyrus.miles@gmail.com>
2018-12-17 12:19:06 -0700

    Now can import folder dir from other classes

--- authentication.py
+++ authentication.py
@@ -1,28 +1,28 @@
-import os
+import os.path
 from searchtools import searchtools
 class authentication(object):
     def __init__(self,root_folder):
-        self.root_folder = root_folder
-        self.showsfolder = root_folder + "shows/"
-        self.showtxt = root_folder + "show_list.txt"
-        self.bookmarktxt = self.showsfolder + "[BOOKMARKED SHOWS].txt"
-        self.logtxt = root_folder + "log.txt"
+        self.dirList = (searchtools(root_folder)).__dict__.values()
 
     def main(self):
-        dirList = self.listOfFolders
-    def dirExists(self,dir):
-        return True if os.isdir(dir) else False
+        for dir in self.dirList:
+            if os.path.exists(dir) is True: continue
+            else:
+                self.makeDir(path=dir)
+            return False
+        return True
 
-    def listOfFolders(self):
-        return [self.root_folder, self.showsfolder, self.showtxt, self.logtxt]
+        # return True
+    def makeDir(self,path=dir):
+        return True if os.isdir(dir) else False
+    def isText(self,dir):
+        pass
+    def isFolder(self,dir):
+        pass
 
 
 projectFolder = os.getcwd()+"/"
 a = authentication(projectFolder)
-print a.root_folder
-instance_variables = a.__dict__.keys()
-s = searchtools(None,projectFolder)
-print type(s)
-print 'kek'
-# for dir in instance_variables:
-#     print s.(dir)
+
+print a.main()
+
