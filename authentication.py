0e6a0a3acb6a219dbc2ef279cb65e7033d551636
haustle <tyrus.miles@gmail.com>
2018-12-17 15:45:39 -0700

    Now replacing missing files

--- authentication.py
+++ authentication.py
@@ -1,28 +1,45 @@
 import os.path
+import os
 from searchtools import searchtools
 class authentication(object):
     def __init__(self,root_folder):
         self.dirList = (searchtools(root_folder)).__dict__.values()
 
     def main(self):
-        for dir in self.dirList:
-            if os.path.exists(dir) is True: continue
-            else:
-                self.makeDir(path=dir)
-            return False
-        return True
 
-        # return True
-    def makeDir(self,path=dir):
-        return True if os.isdir(dir) else False
+        for dir in self.dirList:
+            exists = os.path.exists(dir)
+            
+                
+            if os.path.exists(dir) is False:
+                print("Dir exists: {}\n").format("Missing", dir)
+                filename = (dir.split("/"))[-1]
+
+                if self.isText(filename) is True: 
+                    f = open(dir,'w')
+                    f.close()
+                    
+                else:
+                    os.mkdir(dir)
+                    
+                print("\t[!]Missing \'{}\' --> [+]Added \'{}\'").format(filename,filename)
+                print("\tFilepath: {}").format(dir)
+                print ''
+            
+
     def isText(self,dir):
-        pass
-    def isFolder(self,dir):
-        pass
+        try:
+            broke = dir.split(".")
+            extension = broke[-1]
+            if extension == 'txt': return True
+            return False
+        except:
+            print 'Error: Something went wrong trying to identify the directory'
+
 
 
 projectFolder = os.getcwd()+"/"
-a = authentication(projectFolder)
+# a = authentication(projectFolder)
 
-print a.main()
+authentication(projectFolder).main()
 
