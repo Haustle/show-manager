5ab2ef841ed7c95d1d9038ef8838019d0c7ebc03
haustle <tyrus.miles@gmail.com>
2018-12-17 03:02:00 -0700

    Troubleshoot if user is missing major files

---
+++ authentication.py
@@ -0,0 +1,23 @@
+import os
+from searchtools import searchtools
+class authentication(object):
+    def __init__(self,root_folder):
+        self.root_folder = root_folder
+        self.showsfolder = root_folder + "shows/"
+        self.showtxt = root_folder + "show_list.txt"
+        self.bookmarktxt = self.showsfolder + "[BOOKMARKED SHOWS].txt"
+        self.logtxt = root_folder + "log.txt"
+
+    def main(self):
+        dirList = self.listOfFolders
+    def dirExists(self,dir):
+        return True if os.isdir(dir) else False
+
+    def listOfFolders(self):
+        return [self.root_folder, self.showsfolder, self.showtxt, self.logtxt]
+
+
+projectFolder = os.getcwd()+"/"
+a = authentication(projectFolder)
+print a.root_folder
+print a.__dict__.keys()
