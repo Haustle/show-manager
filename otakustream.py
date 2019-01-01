d4048ed0d66ef5da468de109f8432f99596f9640
haustle <tyrus.miles@gmail.com>
2019-01-01 15:25:03 -0700

    grabs openload mp4 links :D

--- otakustream.py
+++ otakustream.py
@@ -1,7 +1,9 @@
-import webbrowser
-import requests
+import webbrowser ,requests, config, os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome impor
 from bs4 import BeautifulSoup
+import time
 
 def otakuLink(original_name):
     name = "+".join(original_name.split(" "))
@@ -11,6 +13,7 @@ def otakuLink(original_name):
     page = result.text
     
     soup = BeautifulSoup(page,'html.parser')
+    
     
     shows = soup.find_all('div', class_="caption-category")
     link_to_show = None
@@ -27,39 +30,90 @@ def otakuLink(original_name):
         return None
 
 def chooseDownload(download_page_link):
-    
-    result = requests.get(download_page_link)
-    page = result.text
+
+    options = Options()
+    options.headless = True
+
+    driver = webdriver.Chrome('F:\Program Files (x86)\Google\Chrome\Application\chromedriver',chrome_options=options)
+
+    driver.get('https://otakustream.tv/user/login/')
+    driver.find_element_by_id("user_login").send_keys(config.otaku_login)
+    driver.find_element_by_id("user_pass").send_keys(config.otaku_pass)
+    driver.find_element_by_id("wp-submit").click()
+
+    time.sleep(1)
+    driver.get('https://otakustream.tv/user/download/?episode_id=5385')
+    time.sleep(1)
+
+    page = (driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")).encode("utf-8")
     soup = BeautifulSoup(page,'html.parser')
     server_links = soup.find_all('table', class_="table table-striped table-hover")
-    print server_links
+    tds = server_links[0].find_all("td")
+    link = None
+    for x in range (len(tds)):
+        currentd = tds[x].text
+        if currentd == 'Openload': link = tds[x+1].text
 
-    
+    return link
+
+
+
+def openloadMP4(link):
+    chrome_options = Options()
+    chrome_options.add_extension('C:\Users\haust\Desktop\uBlock-Origin_v1.17.4.crx')
+    # chrome_options.add_argument('window-size=1920x1080')
+    chrome_options.headless = False
+
+    driver = webdriver.Chrome('F:\Program Files (x86)\Google\Chrome\Application\chromedriver', chrome_options=chrome_options)
+    driver.get(link)
+    for x in range(3):
+        # print 'This is run through number: {}'.format(x)
+        try:
+            driver.find_element_by_id("videooverlay").click()
+            # print 'I just tried to click'
+        except:
+            continue
+            # print 'The click failed'
+        finally:
+            time.sleep(1)
+
+    page = (driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")).encode("utf-8")
+    soup = BeautifulSoup(page,'html.parser')
+    instantlink = soup.find_all('video', class_="vjs-tech")
+    driver.close()
+    mp4Link = instantlink[0]['src']
+    link = 'https://openload.co{}'.format(mp4Link)
+
+    return link
+
 def otakuDownloadPage(show_link,ep_number):
     ep_string = "episode-{}/".format(str(ep_number))
     base_url = show_link + ep_string
-
     result = requests.get(base_url)
     page = result.text
-
     soup = BeautifulSoup(page,'html.parser')
-
     download_links = soup.find_all('a', title="Download episode")
     downloadlink = download_links[0]['href']
     base_url = 'http://www.otakustream.tv/user'
     final_url = base_url+downloadlink
     
-    # print 'This is the final url: {}'.format(final_url)
-    webbrowser.open(final_url)
     return final_url
 
-
+def selenium_try():
+   pass
 
 
 def main():
+    vlc_dir = 'C:\Program Files\VideoLAN\VLC\\'
     show_link = otakuLink("Your lie in april")
-    server_links = otakuDownloadPage(show_link,5)
-    chooseDownload(server_links)
+    server_links = otakuDownloadPage(show_link,2)
+    openloadLink = chooseDownload(server_links)
+    print 'hello'
+    mp4 = openloadMP4(openloadLink)
+    print mp4
+    os.system('cd {}'.format(vlc_dir))
+    os.system('vlc.exe {}'.format(mp4))
+    
 
 
 main()
